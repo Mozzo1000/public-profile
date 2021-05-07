b0c554ed1a65fe64c428d1ebbd9c68978e8427a4
Mozzo1000 <andreas@simplymozzo.se>
2021-05-07 15:19:06 +0200

    Change navbar home link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,11 +4,11 @@ import { Navbar, Nav, Form, Button } from 'react-bootstrap';
 export default function NavbarMenu() {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#">public-profile</Navbar.Brand> 
+            <Navbar.Brand href="/">public-profile</Navbar.Brand> 
             <Navbar.Toggle aria-controls="navbar" />
             <Navbar.Collapse id="navbar">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#">Home</Nav.Link> 
+                    <Nav.Link href="/">Home</Nav.Link> 
                 </Nav>
                 {localStorage.getItem('auth') &&
                     <Form inline>
